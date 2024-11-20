58430ef563c8dc09add65a4a483f879ae39c7c87
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2024-11-20 23:12:24 +0100

    analysis issue fix

--- templates/app/lib/wrapper.dart
+++ templates/app/lib/wrapper.dart
@@ -2,72 +2,35 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
-import 'package:example_app/core/services/native/navigation/native_navigation_service.dart';
+import 'package:go_router/go_router.dart';
 
-class Wrapper extends StatelessWidget {
-  final Widget child;
-  
-  const Wrapper({
-    super.key,
-    required this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) => PopScope(
-    canPop: false,
-    key: UniqueKey(),
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        pageTransitionsTheme: const PageTransitionsTheme(
-          builders: {
-            TargetPlatform.android: NoTransitionsBuilder(),
-            TargetPlatform.iOS: NoTransitionsBuilder(),
-            TargetPlatform.macOS: NoTransitionsBuilder(),
-            TargetPlatform.windows: NoTransitionsBuilder(),
-            TargetPlatform.linux: NoTransitionsBuilder(),
-          },
-        ),
-      ),
-      builder: (context, child) => FTheme(
-        data: FThemes.zinc.light,
-        child: ValueListenableBuilder<NavigationBar?>(
-          valueListenable: NativeNavigationService.navigationBar,
-          builder: (context, navigationBar, child) => FScaffold(
-            content: !kIsWeb & Platform.isIOS || Platform.isAndroid
-              ? PrimaryScrollController(
-                  controller: ScrollController(),
-                  child: Builder(
-                    builder: (context) => Scrollbar(
-                      controller: PrimaryScrollController.of(context),
-                      interactive: true,
-                      child: child!,
-                    ),
-                  ),
-                )
-              : child!,
-            footer: !Platform.isIOS ? navigationBar : null,
-            contentPad: false,
-          ),
+Page<dynamic> wrapper({
+  required Widget child,
+  bool? isModal = false,
+}) {
+  Widget wrappedChild = child;
+  if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+    wrappedChild = PrimaryScrollController(
+      controller: ScrollController(),
+      child: Builder(
+        builder: (context) => Scrollbar(
+          controller: PrimaryScrollController.of(context),
+          interactive: true,
           child: child,
         ),
       ),
-      home: child,
+    );
+  }
+
+  final content = FTheme(
+    data: FThemes.zinc.light,
+    child: FScaffold(
+      content: wrappedChild,
+      contentPad: false,
     ),
   );
-}
 
-class NoTransitionsBuilder extends PageTransitionsBuilder {
-  const NoTransitionsBuilder();
-  
-  @override
-  Widget buildTransitions<T>(
-    PageRoute<T> route,
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child,
-  ) {
-    return child;
-  }
+  return isModal!
+      ? NoTransitionPage(child: MaterialApp(home: content))
+      : MaterialPage(child: content);
 }
